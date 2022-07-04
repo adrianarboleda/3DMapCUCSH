ba9f89117f552953b03bea51e06fb8062c3b00fb
adrianarboleda <adrian.arboleda@alumnos.udg.mx>
2022-07-03 20:49:39 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        Debug.Log(player.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomController : MonoBehaviour
+{
+    public GameObject selectedObject;
+    public Color selected;
+    public Color notSelected;
+
+    void Start()
+    {
+        selectedObject = GameObject.Find("PlantaBaja");
+        selected = new Color(254, 0, 0, 0.3f);
+        notSelected = new Color(160, 160, 160, 0.2f);
+        Debug.Log("roomControllerStart");
+    }
+
+    void Update()
+    {
+        if( Input.GetMouseButtonDown(0) )
+        {
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+            RaycastHit hit;
+            
+            if( Physics.Raycast( ray, out hit, 100 ) )
+            {
+                
+                Debug.Log(selectedObject.name);
+                Debug.Log(hit.transform.gameObject.name);
+                if (selectedObject.name != hit.transform.gameObject.name) {
+                    Debug.Log("Is not selected");
+                    selectedObject.GetComponent<Renderer>().material.color = notSelected;
+
+                    hit.transform.gameObject.GetComponent<Renderer>().material.color = selected;
+                    selectedObject =  hit.transform.gameObject;
+                }
+            }
+        }
+    }
+}
